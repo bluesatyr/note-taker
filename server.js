57de5599d6e17689a82e06b8e7fa8511753374db
bluesatyr <shawnevans.music@gmail.com>
2021-02-19 17:29:34 -0500

    update PORT for heroku

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const db = require('./db/db.json');
 
 const app = express();
-const PORT = 3001;
+onst PORT = process.env.PORT || 3001;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
