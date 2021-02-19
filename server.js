82d2cfc48ce0faa65ce1cee8c6e9dd7a5db3be95
bluesatyr <shawnevans.music@gmail.com>
2021-02-19 18:01:17 -0500

    create POST route and function to handle creation of new note
    incorporate uuid to create unique note identifiers

--- server.js
+++ server.js
@@ -5,13 +5,32 @@ const path = require('path');
 const db = require('./db/db.json');
 
 const app = express();
-onst PORT = process.env.PORT || 3001;
+const PORT = 3001;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
+/* Functions used in Routes */
+function findById(id, notesArray) {
+  const result = notesArray.filter(note => animal.id === id)[0];
+  return result;
+}
+
+
+function createNewNote(body, notesArray) {
+  const note = body;
+  notesArray.push(note);
+  fs.writeFileSync(
+    path.join(__dirname, './db/db.json'),
+    JSON.stringify({note: notesArray}, null, 2)
+  );
+  return note;
+}
+
+
+/* Routes */
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
   });
@@ -26,11 +45,13 @@ app.get('/api/notes', (req, res) => {
 });
 
 app.get('/api/notes/:id', (req, res) => {
-  //
+  
 });
 
 app.post('/api/notes', (req, res) => {
-    //
+  req.body.id = uuidv4();
+  const note = createNewNote(req.body, db);
+  res.json(note);
 });
 
 app.delete('/api/notes', (req, res) => {
